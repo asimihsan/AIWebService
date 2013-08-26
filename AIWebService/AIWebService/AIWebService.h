6047e4b2109435068f81c7aa3ac184131b3ac210
Asim Ihsan <angry.blade5619@ihsan.io>
2013-08-26 17:28:07 -0500

    initial commit - start TDD of WSDLProxy
    Going to use NSXMLParser rather than a third-party library.

---
+++ AIWebService/AIWebService/AIWebService.h
@@ -0,0 +1,31 @@
+// =============================================================================
+//    AIWebService.h
+//    Use web services that publish their interface using a Web Service
+//    Description Language (WSDL) file over Simple Object Access Protocol
+//    (SOAP).
+//
+//    Project: AIWebService
+// =============================================================================
+//    Created by Asim Ihsan on 2013-08-26.
+//    Version 0.1
+//
+//    Copyright 2013 Asim Ihsan
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// =============================================================================
+
+#import <Foundation/Foundation.h>
+
+@interface AIWebService : NSObject
+
+@end
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '6.0'
+xcodeproj 'AIWebService/AIWebService.xcodeproj'
+pod 'ASIHTTPRequest', '~> 1.8.1'
+pod 'ConciseKit', '~> 0.1.2'
